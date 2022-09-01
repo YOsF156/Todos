eba6e678f4064fa80c6e14a133a1283bb77dd735
YOsF156 <yosistar@gmail.com>
2022-09-02 00:15:35 +0300

    cardentials disable

--- src/stores/api/api.js
+++ src/stores/api/api.js
@@ -5,7 +5,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://todos-server-yosef.herokuapp.com/api",
-    withCredentials: true,
     headers: { "Content-Type": "application/json", }
 });
 
